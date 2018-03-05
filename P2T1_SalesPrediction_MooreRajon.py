68d28f8a066ba6aa0d0ee02b204b971edf032819
MooWasHere <37052845+MooWasHere@users.noreply.github.com>
2018-03-04 19:09:52 -0500

    Add files via upload

---
+++ P2T1_SalesPrediction_MooreRajon.py
@@ -0,0 +1,8 @@
+#Get the projected total sales.
+total_sales =float(input('Enter the projected Sales: '))
+
+#Calculate the profit as 23 percent of the total sales.
+profit = total_sales * 0.23
+
+#Display the profit.
+print('The profit is $', format(profit, '.2f'))
